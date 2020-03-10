dcd9b59793fe8b02b76fd7339db66a62b5c689d9
mluzuriaga <mati.luzuriaga@gmail.com>
2020-03-10 00:50:00 -0300

    Cambio repository

--- src/main/java/com/mercadopago/repositories/WeatherForecastRepository.java
+++ src/main/java/com/mercadopago/repositories/WeatherForecastRepository.java
@@ -1,16 +1,12 @@
 package com.mercadopago.repositories;
 
 import com.mercadopago.model.weatherForecast.WeatherForecast;
-import com.mercadopago.model.weatherForecast.WeatherForecastType;
 import org.springframework.data.repository.CrudRepository;
 
 import java.time.LocalDate;
-import java.util.List;
 
 public interface WeatherForecastRepository extends CrudRepository<WeatherForecast, Long> {
 
     WeatherForecast findAllByDate(LocalDate localDate);
-
-    List<WeatherForecast> findTopByOrderByWeatherForecastType(WeatherForecastType weatherForecastType);
 
 }

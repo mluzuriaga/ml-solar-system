040744ed933bd517b83adcb5a963d26fec24cf61
mluzuriaga <mati.luzuriaga@gmail.com>
2020-03-10 00:29:06 -0300

    Cron cada 2 horas

--- src/main/java/com/mercadopago/repositories/WeatherForecastRepository.java
+++ src/main/java/com/mercadopago/repositories/WeatherForecastRepository.java
@@ -1,12 +1,16 @@
 package com.mercadopago.repositories;
 
 import com.mercadopago.model.weatherForecast.WeatherForecast;
+import com.mercadopago.model.weatherForecast.WeatherForecastType;
 import org.springframework.data.repository.CrudRepository;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public interface WeatherForecastRepository extends CrudRepository<WeatherForecast, Long> {
 
     WeatherForecast findAllByDate(LocalDate localDate);
+
+    List<WeatherForecast> findTopByOrderByWeatherForecastType(WeatherForecastType weatherForecastType);
 
 }
